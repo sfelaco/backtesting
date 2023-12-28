1622debe012c6b9a5348291a5b3d0a62ab22a2c4
jaggaer-sfelaco <90247417+jaggaer-sfelaco@users.noreply.github.com>
2023-12-28 20:44:20 +0100

    Added new DE40 file

--- backtesting/test/__init__.py
+++ backtesting/test/__init__.py
@@ -21,6 +21,9 @@ DE40_2D_5M =  _read_file('DE40_2d_5m.csv')
 DE40_5Y_5M =  _read_file('DE40_5y_5m.csv')
 """DataFrame of 5M DE40 index data from 20 Dec 2022 to 20 Dec 2023."""
 
+DE40_5M_5M =  _read_file('DE40_1M_5m.csv')
+"""DataFrame of 5M DE40 index data from 20 Nov 2023 to 20 Dec 2023."""
+
 def SMA(arr: pd.Series, n: int) -> pd.Series:
     """
     Returns `n`-period simple moving average of array `arr`.
